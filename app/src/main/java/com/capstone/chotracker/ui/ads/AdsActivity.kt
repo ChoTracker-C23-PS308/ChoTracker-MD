16222ead4f14c6eb0874a0c88a506c5a9030dde4
bintangkhd <bintangkhadafi2772@gmail.com>
2023-05-22 22:36:53 +0700

    Create Ads Page

---
+++ app/src/main/java/com/capstone/chotracker/ui/ads/AdsActivity.kt
@@ -0,0 +1,34 @@
+package com.capstone.chotracker.ui.ads
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.capstone.chotracker.databinding.ActivityAdsBinding
+
+class AdsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAdsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+}
