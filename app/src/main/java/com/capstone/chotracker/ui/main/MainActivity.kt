8f19ede3d750e1ded2dda4f58dcf38e4b696b5f6
raisulwathan <raisulw@mhs.unsyiah.ac.id>
2023-05-30 17:27:46 +0700

    create move fragment Home, Chobot, Profile

--- app/src/main/java/com/capstone/chotracker/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/chotracker/ui/main/MainActivity.kt
@@ -5,18 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import com.capstone.chotracker.R
 import com.capstone.chotracker.databinding.ActivityMainBinding
+import com.capstone.chotracker.databinding.FragmentHomeBinding
+import com.capstone.chotracker.ui.chochat.ChoChatLandingPageFragment
+import com.capstone.chotracker.ui.home.HomeFragment
+import com.capstone.chotracker.ui.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingHome: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bindingHome = FragmentHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(bindingHome.root)
+        replaceFragment(HomeFragment())
+
+        bindingHome.bottomNavigationView.setOnItemSelectedListener {
+        when(it.itemId){
+
+            R.id.home_nav -> replaceFragment(HomeFragment())
+            R.id.chochat_nav -> replaceFragment(ChoChatLandingPageFragment())
+            R.id.profil_nav -> replaceFragment(ProfileFragment())
+
+            else -> {
+
+            }
+
+        }
+            true
+        }
 
         setupView()
+
     }
 
     private fun setupView() {
@@ -30,6 +56,14 @@ class MainActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction =fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.bottomAppBar,fragment)
+        fragmentTransaction.commit()
     }
 
 }
