affdcac121ac5d362b9f365fbb8d462be2583fea
bintangkhd <bintangkhadafi2772@gmail.com>
2023-05-30 18:33:59 +0700

    Create Google Login Method

--- app/src/main/java/com/capstone/chotracker/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/chotracker/ui/login/LoginActivity.kt
@@ -1,18 +1,32 @@
 package com.capstone.chotracker.ui.login
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.activity.result.contract.ActivityResultContracts
+import com.capstone.chotracker.R
 import com.capstone.chotracker.databinding.ActivityLoginBinding
+import com.capstone.chotracker.ui.main.MainActivity
 import com.capstone.chotracker.ui.signup.SignupActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +35,20 @@ class LoginActivity : AppCompatActivity() {
 
         setupView()
         navigateToSignUp()
+
+        // Configure Google Login
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        loginWithGoogle()
+
     }
 
     private fun setupView() {
@@ -50,5 +78,67 @@ class LoginActivity : AppCompatActivity() {
         }
         return super.onKeyDown(keyCode, event)
     }
+
+    private fun loginWithGoogle() {
+        binding.layoutLogin.buttonGoogle.setOnClickListener {
+            val intent = googleSignInClient.signInIntent
+            resultLauncher.launch(intent)
+        }
+    }
+
+    private var resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google login failed", e)
+            }
+        }
+
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "loginWithCredential:success")
+                    val user = auth.currentUser
+                    redirectToMainPage(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "loginWithCredential:failure", task.exception)
+                    redirectToMainPage(null)
+                }
+            }
+    }
+
+    private fun redirectToMainPage(currentUser: FirebaseUser?) {
+        if (currentUser != null){
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        redirectToMainPage(currentUser)
+    }
+
+
+    companion object {
+        private const val TAG = "LoginActivity"
+    }
+
 
 }
