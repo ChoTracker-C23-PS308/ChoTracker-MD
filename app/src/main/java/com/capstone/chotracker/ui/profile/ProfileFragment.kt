6d404bb368fd63a10d47e91371615626d3311a08
bintangkhd <bintangkhadafi2772@gmail.com>
2023-06-04 13:26:56 +0700

    Re-slicing UI Profile Page

--- app/src/main/java/com/capstone/chotracker/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstone/chotracker/ui/profile/ProfileFragment.kt
@@ -6,54 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< HEAD
 import com.capstone.chotracker.databinding.FragmentProfileBinding
 import com.capstone.chotracker.ui.on_boarding.OnBoardingActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-
-class ProfileFragment : Fragment() {
-
-    private var _binding : FragmentProfileBinding? = null
-    private val binding get() = _binding!!
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-
-=======
-import androidx.fragment.app.FragmentTransaction
 import com.capstone.chotracker.R
-import com.capstone.chotracker.databinding.FragmentDetailProfileBinding
-import com.capstone.chotracker.databinding.FragmentProfileBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 
 class ProfileFragment : Fragment() {
-        private var _binding: FragmentProfileBinding? = null
-        private val binding get() = _binding!!
->>>>>>> 4cbdf26be7e2da82cb5f0b739a93fab9d32c25c5
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-<<<<<<< HEAD
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-=======
         // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(layoutInflater)
->>>>>>> 4cbdf26be7e2da82cb5f0b739a93fab9d32c25c5
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-<<<<<<< HEAD
-        auth = FirebaseAuth.getInstance()
-        googleSignInClient = GoogleSignIn.getClient(requireActivity(), GoogleSignInOptions.DEFAULT_SIGN_IN)
-
-        logoutButtonHandler()
-
-=======
         binding.editProfile.setOnClickListener {
             val categoryFragment = DetailProfileFragment()
             val fragmentManager = parentFragmentManager
@@ -67,12 +47,16 @@ class ProfileFragment : Fragment() {
                 commit()
             }
         }
+
+        auth = FirebaseAuth.getInstance()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), GoogleSignInOptions.DEFAULT_SIGN_IN)
+
+        logoutButtonHandler()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
->>>>>>> 4cbdf26be7e2da82cb5f0b739a93fab9d32c25c5
     }
 
 
@@ -90,7 +74,6 @@ class ProfileFragment : Fragment() {
             requireActivity().finish()
         }
     }
-
 
 
 }
