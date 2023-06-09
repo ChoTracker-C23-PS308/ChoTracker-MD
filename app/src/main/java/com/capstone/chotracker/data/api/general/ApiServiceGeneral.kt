18b6c3fb6940fc24d82c6a004b7567d193089334
bintangkhd <bintangkhadafi2772@gmail.com>
2023-06-09 23:28:39 +0700

    Update Interface ApiServiceGeneral

--- app/src/main/java/com/capstone/chotracker/data/api/general/ApiServiceGeneral.kt
+++ app/src/main/java/com/capstone/chotracker/data/api/general/ApiServiceGeneral.kt
@@ -1,11 +1,11 @@
 package com.capstone.chotracker.data.api.general
 
-import com.capstone.chotracker.data.response.article.Response
 import com.capstone.chotracker.data.response.history.AddHistoryResponseModel
 import com.capstone.chotracker.data.response.history.AddImageResponseModel
 import com.capstone.chotracker.data.response.history.HistoryModel
 import com.capstone.chotracker.data.response.history.HistoryResponseModel
-import retrofit2.Call
+import okhttp3.MultipartBody
+import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiServiceGeneral {
@@ -13,26 +13,29 @@ interface ApiServiceGeneral {
     // HISTORY
     @Headers("Content-Type: application/json; charset=UTF-8")
     @GET("history/{uid}")
-    suspend  fun getHistories(
+    suspend fun getHistories(
         @Header("Authorization") token: String,
         @Path("uid") uid: String
     ): HistoryResponseModel
 
     @Headers("Content-Type: application/json; charset=UTF-8")
-    @POST("history/{id}")
-    fun addHistory(
+    @POST("history/{uid}")
+    suspend fun addHistory(
         @Header("Authorization") token: String,
-        @Path("id") id: String,
+        @Path("uid") uid: String,
         @Body data: HistoryModel
     ): AddHistoryResponseModel
 
-    @Headers("Content-Type: application/json; charset=UTF-8")
-    @POST("history/{id}/image")
-    fun addImage(
+    @Multipart
+    @POST("history/{uid}/image")
+    suspend fun addImage(
         @Header("Authorization") token: String,
-        @Path("id") id: String
+        @Path("uid") uid: String,
+        @Part file: MultipartBody.Part
     ): AddImageResponseModel
 
+
+    // ARTICLE
     @Headers("Content-Type: application/json; charset=UTF-8")
     @GET("articles")
     fun getArticle(
